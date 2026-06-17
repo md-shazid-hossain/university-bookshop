d8607bbde0e91ddeae7474ecf279fa5862b1700e
Sadman <sahmed30210@gmail.com>
2026-06-17 10:35:41 +0600

    Checked again that everything is up to date.

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,12 +1,17 @@
 const sequelize = require('../config/db');
 
 const connectDB = async () => {
+  // Try catch formula switch
   try {
     await sequelize.authenticate();
+    //showing result succesfull if connection secure.
     console.log('✅ PostgreSQL connected successfully');
   } catch (error) {
+    //showing result db connection failed if error.
     console.error('❌ DB connection failed:', error);
   }
 };
 
 module.exports = { sequelize, connectDB };
+// complete index part.
+//checking every part is up to date.
